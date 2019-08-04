44b8f4185db106db0a4bcf8e32deeed29adf3c56
Julian Siow <Julian.K.Siow@gmail.com>
2019-08-04 21:54:15 +0200

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+	alert("Sorry, try again.")
+}
